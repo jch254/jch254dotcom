90d6689dab6acfd4f9c4f78e903d2232dd13ce23
Jordan Hornblow <jordan@jch254.com>
2019-05-27 10:42:29 +1000

    Update infra to Terraform 0.12. Add GitHub link.

--- components/Main.js
+++ components/Main.js
@@ -16,21 +16,26 @@ const Main = () => (
     <Heading size={1} big>
       Jordan Hornblow
     </Heading>
-    <Flex>
-      <HeadingLink size={2} href="https://603.nz">
+    <Flex style={{ width: '100%' }}>
+      <HeadingLink size={2} href="https://603.nz" style={{ flex: 1, textAlign: 'right' }}>
         Projects
       </HeadingLink>
       <Heading size={2} px={1}>
         |
       </Heading>
-      <HeadingLink size={2} href="./JordanHornblow-Resume.pdf">
+      <HeadingLink size={2} href="https://github.com/jch254" target="_blank" style={{ flex: 1, textAlign: 'left' }}>
+        GitHub
+      </HeadingLink>
+    </Flex>
+    <Flex style={{ width: '100%' }}>
+      <HeadingLink size={2} href="./JordanHornblow-Resume.pdf" style={{ flex: 1, textAlign: 'right' }}>
         Resume
       </HeadingLink>
       <Heading size={2} px={1}>
         |
       </Heading>
-      <HeadingLink size={2} href="mailto:jordan@jch254.com">
-        Contact/Hire
+      <HeadingLink size={2} href="mailto:jordan@jch254.com" style={{ flex: 1, textAlign: 'left' }}>
+        Contact
       </HeadingLink>
     </Flex>
   </Flex>
